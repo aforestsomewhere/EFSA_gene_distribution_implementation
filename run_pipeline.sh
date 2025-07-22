ba094ea10cf1a40e51a884f8cb283a6fc40ebea5
Katie O'Mahony <61972958+aforestsomewhere@users.noreply.github.com>
2025-07-22 14:24:03 +0100

    Update run_pipeline.sh
    Updated to include completeness as a command-line argument

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
-
+#22/7/25 added completeness as cmd line arg
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 5 ]; then
-  echo "Usage: $0 <working_directory> <species> <input_fasta> <seq_type> <plasmid_usage>"
+if [ "$#" -ne 6 ]; then
+  echo "Usage: $0 <working_directory> <species> <input_fasta> <seq_type> <plasmid_usage> <completeness>"
   exit 1
 fi
 
@@ -11,9 +11,16 @@ species="$2"
 input_fasta="$3"
 seq_type="$4"
 plasmid_usage="$5"
+completeness="$6"
+
+# Check compatibility
+if [ "$completeness" = "incomplete" ] && [ "$plasmid_usage" != "remove" ]; then
+  echo "Error: When completeness is 'incomplete', plasmid_usage must be 'remove'."
+  exit 1
+fi
 
 #copy the input fasta file to the output folder (create it if it does not exist already)
 mkdir -p output
 cp "$input_fasta" output/
 
-./main_script.sh "$species" "$input_fasta" "$seq_type" "$plasmid_usage"
+./main_script.sh "$species" "$input_fasta" "$seq_type" "$plasmid_usage" "$completeness"
