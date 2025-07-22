9bef4449863d526baa1498cbf87d2c3ad0990415
Katie O'Mahony <61972958+aforestsomewhere@users.noreply.github.com>
2025-07-22 14:26:21 +0100

    Update select_genomes.py
    Updated to include completeness as command line argument

--- select_genomes.py
+++ select_genomes.py
@@ -1,20 +1,36 @@
 import pandas as pd
+#22/07/25
+import os
+import argparse
 
-# Load the TSV file
-file_path = 'output/refseq/metadata.tsv'
-metadata_df = pd.read_csv(file_path, sep='\t')
+#rewrite as a function
+def select_genomes_fun(completeness):
+  # Load the TSV file
+  file_path = 'output/refseq/metadata.tsv'
+  metadata_df = pd.read_csv(file_path, sep='\t')
+  os.makedirs('output/refseq', exist_ok=True)
 
-# Filter the dataframe to get rows where assembly_level is not 'Complete Genome'
-incomplete_genomes_df = metadata_df[metadata_df['assembly_level'] != 'Complete Genome']
-complete_genomes_df = metadata_df[metadata_df['assembly_level'] == 'Complete Genome']
+  # Select genomes based on completeness
+  if completeness == "complete":
+    selected_df = metadata_df[metadata_df['assembly_level'] == 'Complete Genome']
+    incomplete_genomes_df = metadata_df[metadata_df['assembly_level'] != 'Complete Genome']
+    output_txt_path = 'output/refseq/non_complete_genomes_ids.txt'
+    incomplete_genomes_df['ssembly_accession'].to_csv(output_txt_path, sep='\t', index=False, header=False)
+  elif completeness == "incomplete":
+    selected_df = metadata_df  # keep all
+    #write empty csv of "non-complete" genomes
+    with open('output/refseq/non_complete_genomes_ids.txt', 'w') as f:
+      pass
+  else:
+      raise ValueError("completeness must be either 'complete' or 'incomplete'")
 
+def main():
+    parser = argparse.ArgumentParser(description='Select genomes to include, based on completeness.')
+    parser.add_argument('--completeness', type=str, required=True, help='complete or incomplete')
 
+    args = parser.parse_args()
 
-# Write the non-complete genome IDs to a text file
-output_txt_path = 'output/refseq/non_complete_genomes_ids.txt'
-incomplete_genomes_df['ssembly_accession'].to_csv(output_txt_path, sep='\t', index=False, header=False)
+    select_genomes_fun(args.completeness)
 
-
-# Save the new metadata file containing only complete genomes
-new_metadata_file_path = 'output/refseq/metadata.tsv'
-complete_genomes_df.to_csv(new_metadata_file_path, sep='\t', index=False)
+if __name__ == '__main__':
+    main()
