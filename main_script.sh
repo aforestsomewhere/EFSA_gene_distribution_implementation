d4d538cf20a7a2b084e1a6b31c619a2b0be8f6a3
Katie O'Mahony <61972958+aforestsomewhere@users.noreply.github.com>
2025-07-23 15:23:38 +0100

    Update main_script.sh
    added specification for output folder

--- main_script.sh
+++ main_script.sh
@@ -1,61 +1,63 @@
 #!/usr/bin/bash
 
+#23/7/25 added output folder specification (working_dir)
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 5 ]; then
-  echo "Usage: $0 <species> <input_fasta> <seq_type> <plasmid_usage> <completeness>"
+if [ "$#" -ne 6 ]; then
+  echo "Usage: $0 <working_dir> <species> <input_fasta> <seq_type> <plasmid_usage> <completeness>"
   exit 1
 fi
 
 # Assign arguments to variables
-species="$1"
-input_fasta="$2"
-seq_type="$3"
-plasmid_usage="$4"
-completeness="$5"
+working_dir="$1"
+species="$2"
+input_fasta="$3"
+seq_type="$4"
+plasmid_usage="$5"
+completeness="$6"
 
 # Download the genomes and save the compressed fasta into a folder
-python3 mops_gen_download_with_error_handling.py -g bacteria -o ./output/ -t4 -n "$species"
+python3 mops_gen_download_with_error_handling.py -g bacteria -o ./"$working_dir"/ -t4 -n "$species"
 
 # removing non complete genomes
 python3 select_genomes.py --completeness "$completeness"
-directory="output/refseq/"
-id_file="output/refseq/non_complete_genomes_ids.txt"
+directory="$working_dir/refseq/"
+id_file="$working_dir/refseq/non_complete_genomes_ids.txt"
 while IFS= read -r id; do
     find "$directory" -type f -name "*$id*" -exec rm -v {} \;
 done < "$id_file"
 
 
 #Creating the genome lists for FastANI
-python3 extract_ref.py --input_file output/refseq/metadata.tsv --output_file output/reference_accession.txt
-directory=output/refseq/
-output_file="output/ref_list.txt"
+python3 extract_ref.py --input_file "$working_dir"/refseq/metadata.tsv --output_file "$working_dir"/reference_accession.txt
+directory="$working_dir/refseq/"
+output_file="$working_dir/ref_list.txt"
 find "$directory" -type f ! -name "metadata.tsv" > "$output_file"
-output_file="output/ref_genome.txt"
-cat output/reference_accession.txt | xargs -I {} find "$directory" -type f -name "*{}*" > "$output_file"
+output_file="$working_dir/ref_genome.txt"
+cat "$working_dir"/reference_accession.txt | xargs -I {} find "$directory" -type f -name "*{}*" > "$output_file"
 
 #run fastani type strain to all
-fastANI --ql output/ref_genome.txt --rl output/ref_list.txt  -o output/ani_one_to_all_sub.txt
+fastANI --ql $working_dir/ref_genome.txt --rl $working_dir/ref_list.txt  -o $working_dir/ani_one_to_all_sub.txt
 #run fastani all to all for creation of matrix
-#fastANI --ql output/ref_list.txt --rl output/ref_list.txt --matrix -o output/ani_all_strains_allVsAll.txt
+#fastANI --ql $working_dir/ref_list.txt --rl $working_dir/ref_list.txt --matrix -o $working_dir/ani_all_strains_allVsAll.txt
 
 
 # to produce graphical output of the fastani data as heatmap (all strains vs all strains)
 #ANIclustermap -i ani_step/query_genomes/refseq/ -o clustermap_out --fig_width 20 --fig_height 15 --cmap_colors white,orange,red
 
 # Keeping only the genomes with ANI to reference genome of at least 95
-python3 extraction_suitable_genomes.py --input_file output/ani_one_to_all_sub.txt --source_folder output/refseq/ --output_folder output/filtered
+python3 extraction_suitable_genomes.py --input_file "$working_dir"/ani_one_to_all_sub.txt --source_folder "$working_dir"/refseq/ --output_folder "$working_dir"/filtered
 
 # decompressing filtered genomes
-DIRECTORY="output/filtered/"
+DIRECTORY="$working_dir/filtered/"
 for file in "$DIRECTORY"*.gz; do
     gunzip "$file"
 done
 
-#22/07/25 artificially concatenating incomplete genomes for easier data viz
+#artificially concatenating incomplete genomes for easier data viz
 #added script concatenate_fasta.py to repo
 if [ "$completeness" = "incomplete" ]; then
     input_dir="$DIRECTORY"
-    output_comb="output/filtered_combined"
+    output_comb="$working_dir/filtered_combined"
     mkdir -p "$output_comb"
 
     for file in "$input_dir"/*.fna; do
@@ -69,13 +71,14 @@ if [ "$completeness" = "incomplete" ]; then
   mv "$output_comb"/*.fna "$input_dir/"
 fi
 
-python3 suitable_genomes_plasmids.py --input_directory output/filtered --output_directory output/filtered_plasmids --plasmid_setting "$plasmid_usage"
+
+python3 suitable_genomes_plasmids.py --input_directory "$working_dir"/filtered --output_directory "$working_dir"/filtered_plasmids --plasmid_setting "$plasmid_usage"
 
 # here to chose if plasmid or not
-python3 merge_fasta.py output/filtered_plasmids output/
+python3 merge_fasta.py "$working_dir"/filtered_plasmids "$working_dir"/
 
 # Assign number of genomes to a variable
-directory="output/filtered_plasmids"
+directory="$working_dir/filtered_plasmids"
 genome_count=$(ls -1 "$directory" | wc -l)
 echo "Summary of Variables:"
 echo "Species: $species"
@@ -84,18 +87,17 @@ echo "Seq Type: $seq_type"
 echo "Plasmid Usage: $plasmid_usage"
 echo "Number of filtered genomes: $genome_count"
 
-
-makeblastdb -in output/dict.fasta -out output/blastdb -dbtype nucl -blastdb_version 4
+makeblastdb -in "$working_dir"/dict.fasta -out "$working_dir"/blastdb -dbtype nucl -blastdb_version 4
 
 if [ "$seq_type" = "nucleotide" ]; then
     echo "Running blast for nucleotides"
-    blastn -query "output/${input_fasta}" -db output/blastdb -out output/blast_output.txt -outfmt 6 -num_threads 4 -task blastn -evalue 0.05
+    blastn -query "$working_dir/${input_fasta}" -db "$working_dir"/blastdb -out "$working_dir"/blast_output.txt -outfmt 6 -num_threads 4 -task blastn -evalue 0.05
 elif [ "$seq_type" = "aminoacid" ]; then
     echo "Running blast for proteins"
-    tblastn -query "output/${input_fasta}" -db output/blastdb -out output/blast_output.txt -outfmt 6 -num_threads 4 -evalue 0.05
+    tblastn -query "$working_dir/${input_fasta}" -db "$working_dir"/blastdb -out "$working_dir"/blast_output.txt -outfmt 6 -num_threads 4 -evalue 0.05
 else
     echo "Invalid seq_type. Please specify either 'nucleotide' or 'aminoacid'."
 fi
 
-python3 graphical_out.py --genome_num "$genome_count" --species_name "$species" --blast_out output/blast_output.txt --input_fasta "output/$input_fasta" --identity_cutoff 70 --coverage_cutoff 70
+python3 graphical_out.py --genome_num "$genome_count" --species_name "$species" --blast_out "$working_dir"/blast_output.txt --input_fasta "$working_dir/$input_fasta" --identity_cutoff 70 --coverage_cutoff 70
 
